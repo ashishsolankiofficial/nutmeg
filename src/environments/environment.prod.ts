3b21c53dd1b747c0edd075276b63bb36368d77cd
ashishsolankiofficial <ashishsolanki.acc@gmail.com>
2023-04-06 16:06:51 +0530

    setup link pending

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "http://13.126.165.111/api/",
+  // apiUrl: "http://13.126.165.111/api/",
+  apiUrl: "http://localhost/oj/api/",
   loginUrl: "auth/login/",
   refreshUrl: "auth/token-refresh/",
   resetPasswordUrl: "user/reset-password/",
